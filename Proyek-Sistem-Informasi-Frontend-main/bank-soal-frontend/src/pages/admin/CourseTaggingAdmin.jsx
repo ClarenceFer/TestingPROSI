f248eab4beba9669bac96aa6890d3cf2a60b8877
Clarence <ferdinandclarence5@gmail.com>
2025-10-03 21:34:30 +0700

    Memperbaiki tuilsan

--- Proyek-Sistem-Informasi-Frontend-main/bank-soal-frontend/src/pages/admin/CourseTaggingAdmin.jsx
+++ Proyek-Sistem-Informasi-Frontend-main/bank-soal-frontend/src/pages/admin/CourseTaggingAdmin.jsx
@@ -543,12 +543,12 @@ const CourseTaggingAdmin = ({ currentUser }) => {
       <main className="max-w-7xl mx-auto px-6 py-8">
         {/* Breadcrumb */}
         <div className="flex items-center space-x-2 mb-6">
-          <button
+          {/* <button
             onClick={handleBackToCourses}
             className={`text-sm font-medium ${currentView === 'courses' ? 'text-blue-600' : 'text-gray-500 hover:text-gray-700'} transition-colors`}
           >
             Mata Kuliah
-          </button>
+          </button> */}
           {selectedCourse && (
             <>
               <ChevronRight className="w-4 h-4 text-gray-400" />
